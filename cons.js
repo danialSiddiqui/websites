3b348ade7f3c606f9005d64256b537d17dab7e01
danialSiddiqui <danial_13@hotmail.com>
2021-01-04 09:09:20 +0500

    Comment

---
+++ cons.js
@@ -0,0 +1,107 @@
+const Acc = req.body.Acc;
+const Addmath = req.body.Addmath;
+const AddmissionCredoCollage = req.body.AddmissionCredoCollage;
+ const Age = req.body.Age;
+const Applyingfor = req.body.Applyingfor;
+
+const Bio = req.body.Bio;
+const Bus = req.body.Bus;
+
+const CAddress = req.body.CAddress;
+const CNIC = req.body.CNIC;
+const CPname = req.body.CPname;
+const CareerChoice = req.body.CareerChoice;
+const Che = req.body.Che;
+const CredoCollage = req.body.CredoCollage;
+const Cs = req.body.Cs;
+
+const DoB = req.body.DoB;
+
+const EM = req.body.EM;
+const Eco = req.body.Eco;
+const Enail = req.body.Enail;
+const Englag = req.body.Englag;
+const Englit = req.body.Englit;
+const Ephone = req.body.Ephone;
+const ExCurricular = req.body.ExCurricular;   
+
+
+const FBAddress = req.body.FBAddress; 
+const FCNIC = req.body.FCNIC;   
+const FEmail = req.body.FEmail;
+const FIndustry = req.body.FIndustry; 
+const FName = req.body.FName;
+const FOccution = req.body.FOccution;
+const FOfficeNO = req.body.FOfficeNO;
+const FPhone = req.body.FPhone;
+const FStatus = req.body.FStatus;
+const Familylanguage = req.body.Familylanguage;
+
+const Gender = req.body.Gender;
+  
+const Health = req.body.Health;   
+    
+const Isl = req.body.Isl; 
+
+const MBAddress = req.body.MBAddress;
+const MCNIC = req.body.MCNIC;
+const MEmail = req.body.MEmail;
+const MName = req.body.MName;
+const MOccution = req.body.MOccution;
+const MOfficeNO = req.body.MOfficeNO;
+const MIndustry = req.body.MIndustry;
+const MPhone = req.body.MPhone;
+const MStatus = req.body.MStatus;
+const Math = req.body.Math;
+
+const PAddress = req.body.PAddress;
+const PS = req.body.PS;
+const Phone = req.body.Phone;
+const Phy = req.body.Phy;
+
+const Name = req.body.Name;
+   
+   const Regno = req.body.Regno;
+  
+   
+      const Nationality = req.body.Nationality;
+   
+   
+   
+
+   
+   
+ 
+   const Sname = req.body.Sname;
+   const Relationship = req.body.Relationship;
+  
+   const NOI = req.body.NOI;
+   const CPname = req.body.CPname;
+
+   const CpRelationship = req.body.CpRelationship;
+   const Subject1 = req.body.Subject1;
+   const Subject2 = req.body.Subject2;
+   const Subject3 = req.body.Subject3;
+   const Subject4  = req.body.Subject4;
+   
+   
+   
+   
+   
+   
+   const Soc = req.body.Cs;
+   
+   
+   
+   const Urdu = req.body.Urdu;
+  
+  
+   
+  
+   const ShortParah = req.body.ShortParah;
+   const Sports = req.body.Sports;
+   const Responsiblites = req.body.Responsiblites;
+   
+   
+   
+   const SiblingCredoCollage = req.body.SiblingCredoCollage;
---
+++ ss.js
@@ -0,0 +1,80 @@
+"Acc": "test",
+"Addmath": "test",
+"AddmissionCredoCollage": "test",
+"Age": "test",
+"Applyingfor": "test",
+
+"Bio": "test",
+"Bus": "test",
+
+"CAddress": "test",
+"CNIC": "test",
+"CPname": "test",
+"CareerChoice": "test",
+"Che": "test",
+"CredoCollage": "test",
+"Cs": "test",
+
+"DoB": "test",
+
+"EM": "test",
+"Eco": "test",
+"Enail": "test",
+"Englag": "test",
+"Englit": "test",
+"Ephone": "test",
+"ExCurricular": "test",
+
+"FBAddress": "test",
+"FCNIC": "test",
+"FEmail": "test",
+"FIndustry": "test",
+"FName": "test",
+"FOccution": "test",
+"FOfficeNO": "test",
+"FPhone": "test",
+"FStatus": "test",
+"Familylanguage": "test",
+
+"Gender": "test",
+
+"Health": "test",
+
+"Isl": "test",
+
+"MBAddress": "test",
+"MCNIC": "test",
+"MEmail": "test",
+"MIndustry": "test",
+"MName": "test",
+"MOccution": "test",
+"MOfficeNO": "test",
+"MPhone": "test",
+"MStatus": "test",
+"Math": "test",
+
+"NOI": "test",
+"Name": "test",
+"Nationality": "test",
+
+"PAddress": "test",
+"PS": "test",
+"Phone": "test",
+"Phy": "test",
+
+"Regno": "test",
+"Relationship": "test",
+"Responsiblites": "test",
+
+"ShortParah": "test",
+"SiblingCredoCollage": "test",
+"Sname": "test",
+"Soc": "test",
+"Sports": "test",
+"Subject1": "test",
+"Subject2": "test",
+"Subject3": "test",
+"Subject4": "test",
+
+"Urdu": "test"
+}
